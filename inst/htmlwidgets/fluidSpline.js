800641e63f4350a76170f4d50b68056f7ffc4d89
yonicd <yonicd@gmail.com>
2017-03-01 19:52:56 -0500

    add tooltips and axes

--- inst/htmlwidgets/fluidSpline.js
+++ inst/htmlwidgets/fluidSpline.js
@@ -25,22 +25,44 @@ HTMLWidgets.widget({
         aForm.appendChild(aLabel);
         aForm.appendChild(aSelect);
         
-        var width = x.width,
-            height = x.height;
+var margin={top:20,right:20,bottom:30,left:40},width = x.width-margin.left-margin.right,
+    height = x.height-margin.top-margin.bottom;
+ 
 
         var points = d3.range(1, x.n).map(function(i) {
           return [(data.x[i]-1) * width / x.n, 50 + data.y[i] * (height - 100)];
         });
+        
+        // setup x 
+var xValue = function(d) { return d[0];}, // data -> value
+    xScale = d3.scale.linear().range([0, width]), // value -> display
+    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+// setup y
+var yValue = function(d) { return d[1];}, // data -> value
+    yScale = d3.scale.linear()
+    .range([height, 0]), // value -> display
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    yAxis = d3.svg.axis().scale(yScale).orient("left");  
+
+// add the tooltip area to the webpage
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
         
         var dragged = null,
             selected = points[0];
         
         var line = d3.svg.line();
         
-        var svg = d3.select(el).append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("tabindex", 1);
+var svg = d3.select(el).append("svg")
+    .attr("width", width+margin.left+margin.right)
+    .attr("height", height+margin.top+margin.bottom).append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("tabindex", 1);
+
         
         svg.append("rect")
             .attr("width", width)
@@ -76,8 +98,31 @@ HTMLWidgets.widget({
             .attr("value", function(d) { return d; })
             .text(function(d) { return d; });
         
-        svg.node().focus();
-        
+        //svg.node().focus();
+          // x-axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("X axis");
+
+  // y-axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Y axis");   
+  
         function redraw() {
           svg.select("path").attr("d", line);
         
@@ -91,7 +136,20 @@ HTMLWidgets.widget({
               .duration(750)
               .ease("elastic")
               .attr("r", 6.5);
-        
+        circle.on("mouseover", function(d) {
+          tooltip.transition()
+               .duration(200)
+               .style("opacity", 0.9);
+          tooltip.html("(" + d3.round(xValue(d),1) + ", " + 		d3.round(yValue(d),1) + ")")
+               .style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY) + "px");
+      })
+      .on("mouseout", function(d) {
+          tooltip.transition()
+               .duration(500)
+               .style("opacity", 0);
+      });
+      
           circle
               .classed("selected", function(d) { return d === selected; })
               .attr("cx", function(d) { return d[0]; })
